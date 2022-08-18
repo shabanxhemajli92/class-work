a890a0592a023b002d60e748b2a525a2ee7c58ad
Shaban <shabanxhemajli@live.com>
2022-08-18 09:39:49 +0200

    first commit

---
+++ demo.py
@@ -0,0 +1,12 @@
+#When programming in Python, indentation is something that you will definitely use. However, you should be careful with it, as it can lead to syntax errors. The recommendation is therefore to use 4 spaces for indentation. For example, this statement uses 4 spaces of indentation:
+'''if True:
+    print("It works")'''
+#Also for this for loop with print statment is intended with 4 spaces.
+'''for element in range(0, 5):
+    print(element)'''
+#When you write a big expression, it is best to keep the expression vertically aligned. When you do this, you'll create a "hanging indent". Here is an example of the hanging indent in big expressions, which show some variations of how you can use it:
+def square_of_number(
+     num1, num2, num3, 
+     num4):
+ return num1**2, num2**2, num3**2, num4**2
+   
